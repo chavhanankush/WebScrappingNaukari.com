cc686a0d40e76373acbadb31ce2e0d595d0fd1e8
chavhanankush <ankush.chavhan@neosoftmail.com>
2022-12-07 14:48:50 +0530

    main file

---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+# print(soup)
+print(soup.prettify)
+
+job_elements = soup.find_all("div", class_="card-content")
+print(soup.find_all)
+
+for job_element in job_elements:
+    print(job_element, end="\n"*2)
+
+
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element)
+    print(company_element)
+    print(location_element)
+    print()
+
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text)
+    print(company_element.text)
+    print(location_element.text)
+    print()
+
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    print()
+
+
+python_jobs = soup.find_all("h2", string="Python")
+print(python_jobs)
+
+python_jobs = soup.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
+
+print(len(python_jobs))
+
+python_jobs = soup.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
+
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
